35bf2d388e5c3bce5ac69a808ea7dbc3ed75372d
Duhor <dusanhorak01@seznam.cz>
2025-10-02 14:18:06 +0300

    3.3_done

--- ex3/currCtrl.m
+++ ex3/currCtrl.m
@@ -16,16 +16,49 @@ ukm1_abc = inVec(10:12);   	% previous three-phase switch position applied at ti
 
 % turn the inputs into the per unit system and the stationary orthogonal 
 % reference frame
-% **** to be added ****
+ab_ikp1_ref = Par.K* ikp1_ref_abc/Par.Base.I; 	% three-phase current reference at time step k+1
+ab_ik =Par.K*ik_abc/Par.Base.I  ;        % three-phase current measurement at time step k
+ab_vgk =Par.K* vgk_abc/Par.Base.V;    	% three-phase grid voltage measurement at time step k
+
 
 % implement the predictive current controller
 % **** to be added ****
-    
+sm=sum(abs(ukm1_abc))+1;% 0 1 2 3 +1
+pole_pocet=[3*3*3, 3*3*2, 3*2*2, 2*2*2];
+n=pole_pocet(sm);
+
+iter=1;
+pos_u=zeros(n,3);
+
+for i=-1:1
+    for j=-1:1
+        for k=-1:1
+            tmp=[i,j,k];
+            if(max(abs(tmp-ukm1_abc'))<2)
+                pos_u(iter,:)=tmp;
+                iter=iter+1;
+            end
+            
+        end
+    end
+end
+
+J=ones(size(pos_u,1),1);
+ik1_pred=zeros(2,n);
+for i=1:n
+    ik1_pred(:,i)=Par.Ctr.A*ab_ik+Par.Ctr.B1*pos_u(i,:)'+Par.Ctr.B2*ab_vgk;
+    J(i)=norm(ik1_pred(:,i)-ab_ikp1_ref,2)^2+Par.Ctr.lambdaU *norm(ukm1_abc-pos_u(i,:)',1);
+end
+[err,index]=min(J);
+err
+uk_abc=pos_u(index,:);
+
 % for the chosen switch position: 
 % compute the predicted current in pu and alpha/beta at the next time step
-ikp1 = [0; 0];  % **** to be added ****
+% ikp1 = ik1_pred(:,index)  % **** to be added ****
+ikp1= ik1_pred(:,index);
 
 % output interface
-outVec = [uk_abc; ikp1];
+outVec = [uk_abc'; ikp1];
     
 return

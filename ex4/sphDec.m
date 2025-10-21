d59c6d0ca220486cdf6a9fa610301a2bdf6f0087
Duhor <dusanhorak01@seznam.cz>
2025-10-21 15:22:24 +0300

    transfer check

--- ex4/sphDec.m
+++ ex4/sphDec.m
@@ -1,5 +1,5 @@
 function [U, Uopt, rho, searchFlag] = sphDec(U, Uopt, dist, i, iMax, rho, Ubar_unc, V, searchFlag, u_km1)
-
+print =1;
 % basic sphere decoder
 %     
 % U:        switching sequence with -1,0,1 elements
@@ -19,25 +19,45 @@ function [U, Uopt, rho, searchFlag] = sphDec(U, Uopt, dist, i, iMax, rho, Ubar_u
 for u = -1:1:1
     U(i) = u;         % to be added
     d = norm(Ubar_unc(1:i)- V(1:i,1:i)*U(1:i),2)^2 + dist;     % to be added
+   
     
+    % check if transfer is legal
+    valid_transefer=1;
+    if i>3
+        if 1<(U(i)-U(i-3))
+            valid_transefer=0;
+            if print
+                fprintf("ivalid transfer detected \n")
+            end
+        end
+    end
+
     % display node information:
-    for j = 1:i-1, fprintf('   '); end;
-    fprintf('i=%i: U=', i); 
-    fprintf('%i',U(1:i));
-    fprintf(' dist/rho=%1.2f  ...', d/rho);
-    
-    if d<=rho % to be added
+    if print
+        for j = 1:i-1, fprintf('   '); end;
+        fprintf('i=%i: U=', i); 
+        fprintf('%i',U(1:i));
+        fprintf(' dist/rho=%1.2f  ...', d/rho);
+    end
+
+    if d<=rho && valid_transefer % to be added
         if i < iMax
-            fprintf('branch further\n');
+            if print
+                fprintf('branch further\n');
+            end
             [U, Uopt, rho, searchFlag] = sphDec(U, Uopt, d, i+1, iMax, rho, Ubar_unc, V, searchFlag, u_km1); % to be added
         else
-            fprintf('a (better) switching sequence found - tightening by %1.2f percent\n',(rho-d)/d*100);
+            if print
+                fprintf('a (better) switching sequence found - tightening by %1.2f percent\n',(rho-d)/d*100);
+            end
             Uopt = U;
             rho = d;
             searchFlag = searchFlag + 1;            
         end
     else
-      	fprintf('terminate (cut) branch\n');
+        if print
+      	    fprintf('terminate (cut) branch\n');
+        end
     end
 end
 
